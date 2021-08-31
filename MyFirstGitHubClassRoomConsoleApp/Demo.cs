471cbc6003f0a38802a65d44fa53cb686050a6a2
Ziaullah Hassan <Ziaullah Hassan@DESKTOP-2U19JOA>
2021-08-31 05:32:32 -0700

    a

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public String CoffeName { get; set; }
+        public int price { get; set; }
+
+        public Demo()
+        {
+            
+        }
+        
+
+
+    }
+}
